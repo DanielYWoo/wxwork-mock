ff22bd60deb088f7daabb7fe11e378113c892e40
Daniel Wu <daniel.y.woo@gmail.com>
2021-07-21 20:36:42 +0800

    reconnect and heart beat

--- src/main/java/WxMock.java
+++ src/main/java/WxMock.java
@@ -10,9 +10,10 @@ public class WxMock {
 
     private static final Logger logger = LoggerFactory.getLogger("server");
 
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) throws URISyntaxException, InterruptedException {
         System.setProperty("java.util.logging.SimpleFormatter.format","%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$-6s %2$s %5$s%6$s%n");
         String im = args[0];
+        String clientId = im.substring(im.lastIndexOf('/'));
         logger.info("mock started, try to connect to IM:" + im);
         WebSocketClient cc = new WebSocketClient(new URI(im)) {
 
@@ -24,20 +25,40 @@ public class WxMock {
             @Override
             public void onOpen(ServerHandshake handshake) {
                 logger.info("connected to im-server: " + getURI());
+
             }
 
             @Override
             public void onClose(int code, String reason, boolean remote) {
-                System.out.println("disconnected from: " + getURI() + "; Code: " + code + " " + reason);
+                logger.info("disconnected from: " + getURI() + "; Code: " + code + " " + reason);
             }
 
             @Override
             public void onError(Exception ex) {
-                ex.printStackTrace();
-            }
+                logger.error("error on connection", ex);
+           }
+
         };
 
         cc.connect();
+        String data = "{\n" +
+                "  \"type\":10009,\n" +
+                "  \"clientId\":\"" + clientId + "\",\n" +
+                "  \"data\":{\n" +
+                "    \"hardwareInfo\":{},\n" +
+                "    \"content\": \"heartCheck\"\n" +
+                "  }\n" +
+                "}";
+        while (true) {
+            try { Thread.sleep(3000); } catch (InterruptedException e) { e.printStackTrace(); }
+            if (cc.isOpen()) {
+                logger.info("heart beat");
+                cc.send(data);
+            } else {
+                logger.info("sleep 3 seconds to reconnect");
+                cc.reconnect();
+            }
+        }
 
     }
 
